0616f0412ad4df4da29cdc73699b0c778eb58955
gatesikev <gatesikev1@gmail.com>
2026-03-05 01:44:03 -0800

    Add people in people.js

---
+++ data/people.js
@@ -0,0 +1,25 @@
+const people = [
+  {
+    id: 1,
+    name: "Alice Johnson",
+    role: "Software Engineer",
+    department: "Engineering",
+    avatar: "picture1"
+  },
+  {
+    id: 2,
+    name: "Michaela Smith",
+    role: "UI/UX Designer",
+    department: "Design",
+    avatar: "picture2"
+  },
+  {
+    id: 3,
+    name: "Sarah Williams",
+    role: "Project Manager",
+    department: "Management",
+    avatar: "picture3"
+  }
+];
+
+export default people;
